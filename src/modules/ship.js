7dda53d6052179156d5de7236aadfa4d9a1c9cce
Song <songzhang015@gmail.com>
2025-07-23 09:55:23 -0700

    Add ship class and testing

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,1 +1,19 @@
 /* Ship class for ship objects with varying sizes and functions */
+class Ship {
+	constructor(name, length, hitAmount = 0, sunk = false) {
+		this.name = name;
+		this.length = length;
+		this.hitAmount = hitAmount;
+		this.sunk = sunk;
+	}
+
+	hit() {
+		this.hitAmount++;
+	}
+
+	isSunk() {
+		return this.hitAmount >= this.length;
+	}
+}
+
+export { Ship };
--- src/tests/test.test.js
+++
@@ -1,5 +0,0 @@
-import { add } from "../modules/add.js";
-
-test("adds 1 + 1 to equal 2", () => {
-	expect(add(1, 1)).toBe(2);
-});
